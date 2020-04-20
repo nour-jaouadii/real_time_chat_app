8f28065c0a7ba66fc274f00a982c17063d899dfb
unknown <nourjaouadii2@gmail.com>
2020-04-20 18:48:59 +0100

    'Setup'

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+
+import firebase from "firebase";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    
+    // your config application
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+
+firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+// export firestore database
+export default firebaseApp.firestore();
