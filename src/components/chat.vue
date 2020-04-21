9b8a4e631a8b3ae73d1204b7a47405930f49fd0f
unknown <nourjaouadii2@gmail.com>
2020-04-21 05:09:25 +0100

    creating chat window and NewMessage Component

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,15 +1,33 @@
 <template>
     <div class="chat container">
-        <h2>Chat {{ this.name }}</h2>
+        <h2 class="center teal-text "> hello {{ this.name }}</h2>
+         <div class="card">
+             <div class="card-content">
+                 <ul class="messages"></ul>
+                   <li>
+                     <span class="teal-text">Name </span> 
+                     <span class="grey-text text-darken-3">message</span>
+                     <span class="grey-text time">time</span>
+                   </li>
+             </div>
+             <div class="card-action">
+               <NewMessage :name='name' /> 
+             </div>
+         </div>
     </div>
 </template>
 
 
 
 <script>
+import NewMessage from "@/components/NewMessage"
+
 export default {
   name: "welcome",
-     props: ['name'],
+     props: ['name'], // name from welcome  (we recieve a prop from parent component)
+     components: {
+       NewMessage
+     },  
 
   data() {
     return{
@@ -19,6 +37,19 @@ export default {
  
 };
 </script>
-<style>
 
+<style>
+.chat h2 {
+  font-size:2.6em;
+  margin-bottom: 40px;
+} 
+.chat span{
+  font-size: 1.4em;
+
+  }
+ .chat .time{
+   display: block;
+   font-size: 1.2em;
+
+ } 
 </style>
