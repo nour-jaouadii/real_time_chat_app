5f71c9d01fed89bf237ffd15f470bdac486a96ee
unknown <nourjaouadii2@gmail.com>
2020-04-22 01:03:40 +0100

    Real time event(event listener(onsnapshot))

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -3,12 +3,13 @@
         <h2 class="center teal-text "> hello {{ this.name }}</h2>
          <div class="card">
              <div class="card-content">
-                 <ul class="messages"></ul>
-                   <li>
-                     <span class="teal-text">Name </span> 
-                     <span class="grey-text text-darken-3">message</span>
-                     <span class="grey-text time">time</span>
+                 <ul class="messages" > 
+                   <li v-for="message in messages" :key="message.id ">
+                     <span class="teal-text">{{ message.name}} </span> 
+                     <span class="grey-text text-darken-3">{{ message.content}} </span>
+                     <span class="grey-text time">>{{ message.timestamp}} </span>
                    </li>
+                 </ul>
              </div>
              <div class="card-action">
                <NewMessage :name='name' /> 
@@ -21,6 +22,7 @@
 
 <script>
 import NewMessage from "@/components/NewMessage"
+import db from '@/firebase/init'
 
 export default {
   name: "welcome",
@@ -28,11 +30,38 @@ export default {
      components: {
        NewMessage
      },  
+     
 
   data() {
     return{
-         
+         messages:[]
     }
+  },
+  created() {
+    // we want to listen to collection
+    let ref = db.collection('messages').orderBy('timestamp')
+    // setup listener to listen to this collection
+    ref.onSnapshot(snapshot => {
+      // console.log( snapshot.docChanges()) // snapshot database
+      
+      // when we recieve a change of type added  we take document(doc) and we pushing it(doc) to message[]
+       snapshot.docChanges().forEach(change => {
+         if(change.type == 'added'){ // type of change inside the db is added
+          
+             let doc = change.doc  
+             this.messages.push({
+               id : doc.id,
+               name: doc.data().name, 
+               content:doc.data().content,
+               timestamp: doc.data().timestamp
+             })
+         }
+         
+       });
+
+    })
+
+  
   },
  
 };
